ce02f3e458dd046334a13f3f92c4b9728fc874fd
OttawaSTEM <like.ca@gmail.com>
2022-11-22 21:50:35 -0500

    test weather

--- lib/modules/weather/bloc/weather_bloc.dart
+++ lib/modules/weather/bloc/weather_bloc.dart
@@ -11,22 +11,25 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
     on<GetWeatherDataEvent>(_getWeatherData);
   }
 
-  _getWeatherData(event, emit) {
-    emit(WeatherState(
-      temprature: state.temprature + 1,
-    ));
-  }
-
-  // final url =
-  //     'http://api.openweathermap.org/data/2.5/weather?q=ottawa&appid=d885aa1d783fd13a55050afeef620fcb';
-
-  // _getWeatherData(event, emit) async {
-  //   final data = await HttpService().get(url);
-  //   print(data);
+  // _getWeatherData(event, emit) {
   //   emit(WeatherState(
-  //     temprature: data.main.temp,
+  //     temprature: state.temprature + 1,
   //   ));
   // }
+
+  final url =
+      'http://api.openweathermap.org/data/2.5/weather?q=ottawa&appid=d885aa1d783fd13a55050afeef620fcb';
+
+  Future _getWeatherData(event, emit) async {
+    final dynamic data = await HttpService().get(
+        'https://api.openweathermap.org/data/2.5/weather?q=ottawa&appid=d885aa1d783fd13a55050afeef620fcb/');
+    print(data);
+    // if (data) {
+    //   emit(WeatherState(
+    //     temprature: data.main.temp,
+    //   ));
+    // }
+  }
 }
 
 // class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
