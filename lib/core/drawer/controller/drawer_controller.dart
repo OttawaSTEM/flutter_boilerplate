404d033a5cf84101aa2a1987bf272350b27f8cde
OttawaSTEM <like.ca@gmail.com>
2023-08-23 08:51:42 -0400

    remove http_req.dart

--- lib/core/drawer/controller/drawer_controller.dart
+++ lib/core/drawer/controller/drawer_controller.dart
@@ -4,13 +4,14 @@ import 'package:get_storage/get_storage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 import '../../../../utils/rest_api.dart';
-import '../../../../constants/http_req.dart';
 import '../../../pages/home/ui/home.dart';
 import '../../../../widgets/snack_bar_msg.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:logger/logger.dart';
 
 var logger = Logger();
+final env = dotenv.env;
 
 GoogleSignIn _googleSignIn = GoogleSignIn(
   scopes: <String>[
