6bbe4b2bcb6dbc929995a815c7191cb0ff530f3f
OttawaSTEM <ottawastem@gmail.com>
2025-02-17 12:17:02 -0500

    update

--- lib/pages/settings/ui/settings_ui.dart
+++ lib/pages/settings/ui/settings_ui.dart
@@ -10,8 +10,6 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  // final Future controller = GroupsController().getGroups();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +19,6 @@ class _SettingsPageState extends State<SettingsPage> {
       body: SettingsList(
         sections: [
           SettingsSection(
-            title: Text('Common'),
             tiles: <SettingsTile>[
               SettingsTile.navigation(
                 leading: Icon(Icons.language),
@@ -37,37 +34,6 @@ class _SettingsPageState extends State<SettingsPage> {
             ],
           ),
         ],
-      ),
-      // body: FutureBuilder(
-      //   future: controller,
-      //   builder: (BuildContext context, AsyncSnapshot snapshot) {
-      //     if (snapshot.hasError) {
-      //       return Center(child: Text('Some error occurred ${snapshot.error}'));
-      //     } else if (snapshot.hasData) {
-      //       final groups = snapshot.data;
-      //       return ListView.builder(
-      //         itemCount: groups.length,
-      //         itemBuilder: (context, index) {
-      //           Map group = groups[index];
-      //           return ListTile(
-      //             title: Text('${group['name']}'),
-      //             subtitle: Text('${group['age']}'),
-      //             // onTap: () {
-      //             //   Navigator.of(context).push(MaterialPageRoute(builder: (context) => PostDetails(thisItem['id'].toString())));
-      //             // },
-      //           );
-      //         },
-      //       );
-      //     } else {
-      //       return const Center(child: CircularProgressIndicator());
-      //     }
-      //   },
-      // ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          // controller.sendPost();
-        },
       ),
     );
   }
